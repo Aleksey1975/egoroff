c8a8a1d5b18741861b6dbcce2a9f2e4f1070baa2
Alex <4422504a@gmail.com>
2023-06-14 21:34:58 +0300

    '5'

--- goroscope/urls.py
+++ goroscope/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
 
-
-
 urlpatterns = [
     path('', index_gor, name='index_gor'),
+    path('lion/', lion, name='lion'),
+    path('deva/', deva, name='deva'),
+    path('vesi/', vesi, name='vesi'),
+    path('rak/', rak, name='rak'),
 ]
--- goroscope/views.py
+++ goroscope/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
 def index_gor(request):
-    return render(request, 'goroscope/index_gor.html')
+    return render(request, 'goroscope/index_gor.html', {'title':'Гороскоп'})
+
+
+def lion(request):
+    return render(request, 'goroscope/lion.html', {'title':'Лев'})
+
+
+def rak(request):
+    return render(request, 'goroscope/rak.html', {'title':'Рак'})
+
+
+
+def vesi(request):
+    return render(request, 'goroscope/vesi.html', {'title':'Весы'})
+
+
+
+def deva(request):
+    return render(request, 'goroscope/deva.html', {'title':'Дева'})
